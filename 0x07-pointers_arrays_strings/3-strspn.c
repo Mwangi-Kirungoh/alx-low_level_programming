1a4545d4cbe66b3af3a046e2c39ab63b4b35b568
Mwangi-Kirungoh <kirungohbsm@gmail.com>
2023-04-03 15:00:43 -0700

    0x07. C - Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,26 +1,38 @@
 #include "main.h"
+
 /**
 * _strspn - gets the length of a prefix substring
 * @s: pointer to the string to be searched
 * @accept: pointer to the string of accepted characters
-*
 * Return: the number of bytes in the initial segment of s which consist only of bytes from accept
 */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j;
+unsigned int count = 0;
 
-for (i = 0; s[i] != '\0'; i++)
+while (*s != '\0')
 {
-for (j = 0; accept[j] != '\0'; j++)
+int found = 0;
+char *a = accept;
+
+while (*a != '\0')
 {
-if (s[i] == accept[j])
+if (*s == *a)
+{
+count++;
+found = 1;
 break;
 }
-
-if (accept[j] == '\0')
-return (i);
+a++;
 }
 
-return (i);
+if (found == 0)
+{
+return count;
+}
+
+s++;
+}
+
+return count;
 }
